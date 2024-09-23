7363dc25a69448f8f2f79bf0de419ff1a2c7cdf2
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-09-23 10:59:10 +0200

    Added code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Middlewares - Middy
+
+## Övningar
+
+Swing notes API: https://gist.github.com/zocom-christoffer-wallenberg/82677507f8c5abcf195ba5c7f7fad18c
---
+++ example/functions/Account/index.js
@@ -0,0 +1,38 @@
+const { sendResponse, sendError } = require('../../responses/index');
+const { db } = require('../../services/db');
+const middy = require('@middy/core');
+const { validateToken } = require('../../middleware/auth');
+
+async function getAccount(username) {
+    try {
+        const user = await db.get({
+            TableName: "accounts",
+            Key: {
+                username: username
+            }
+        }).promise();
+
+        if (user?.Item) return user.Item;
+        else return false;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const handler = middy()
+    .handler(async (event) => {
+        try {
+            if (!event?.id || (event?.error && event?.error === '401')) return sendError(401, { success: false, message: 'Invalid token' });
+
+            const account = await getAccount(event.username);
+    
+            if (!account) return sendError(401, { success: false, message: 'No account found' });
+    
+            return sendResponse({ success: true, account: account });
+        } catch (error) {
+            return sendError(400, { success: false, message: 'Could not get account' });
+        }
+    })
+    .use(validateToken);
+
+module.exports = { handler }
